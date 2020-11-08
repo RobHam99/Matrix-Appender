d1545821f3526e9f22aeff73f67f889bf2be7855
Robbie Hamilton <71784867+RobHam99@users.noreply.github.com>
2020-11-08 16:31:08 +0000

    Add files via upload

---
+++ matixappender.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def matrix(matlist):
+
+    n = len(matlist[0])
+
+    Aarr = np.array(matlist)
+    block = np.zeros((n ** 2, n ** 2))
+
+    nextmatcol = 0
+    nextmatrow = 0
+
+    endpoint = n
+    colendpoint = n
+
+    for i in range((len(Aarr))):
+
+        if i != 0 and i % n == 0:
+            nextmatrow += n
+            endpoint += n
+
+        else:
+            nextmatrow = nextmatrow
+            endpoint = endpoint
+
+        if i % n == 0:
+            nextmatcol = 0
+            colendpoint = n
+
+        elif i % n != 0:
+            nextmatcol += n
+            colendpoint += n
+
+        Amat = Aarr[i]
+
+        for j, p in zip(range(nextmatrow, endpoint), range(0, n)):
+            for k, q in zip(range(nextmatcol, colendpoint), range(0, n)):
+                block[j, k] = Amat[p, q]
+
+    print(block)
+
+    return block
+
+matlist = [np.array([[1, 0], [2, 2]]), np.array([[4, 6], [7, 5]]), np.array([[19, 22], [6, 16]]), np.array([[5, 4], [7, 7]])]
+
+x = matrix(matlist)
